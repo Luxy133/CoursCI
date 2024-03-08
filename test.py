673a6a125319708db87d884b4b02858a21abdb58
deltaryss <solarylord@gmail.com>
2024-03-08 23:02:18 +0100

    test

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 # test_string_utils.py
 
 import unittest
-from main import StringUtils
+from main import stringCheck
 
 class TestStringUtils(unittest.TestCase):
 
     def test_inverser_chaine(self):
-        self.assertEqual(StringUtils.inverser_chaine("abcd"), "dcba")
+        self.assertEqual(stringCheck.inverser_chaine("abcd"), "dcba")
 
     def test_est_palindrome(self):
-        self.assertTrue(StringUtils.est_palindrome("radar"))
-        self.assertFalse(StringUtils.est_palindrome("python"))
+        self.assertTrue(stringCheck.est_palindrome("radar"))
+        self.assertFalse(stringCheck.est_palindrome("python"))
 
     def test_compter_lettres(self):
-        self.assertEqual(StringUtils.compter_lettres("python"), 6)
+        self.assertEqual(stringCheck.compter_lettres("python"), 6)
 
 if __name__ == "__main__":
     unittest.main()
